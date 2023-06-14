de8908023ef9da5beca7fe675bd33525e55141c3
Syahril Ramadhan <rodyulo20@gmail.com>
2023-06-14 16:36:31 +0700

    Prepare

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("tw-elements/dist/plugin.cjs")
+  ],
+});
