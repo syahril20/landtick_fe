ea32edcb3630625f5dabf98c3295cce66a3c54c3
Syahril Ramadhan <rodyulo20@gmail.com>
2023-06-19 04:02:21 +0700

    Update api.js

--- src/config/api/api.js
+++ src/config/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL : "http://localhost:5000/api/v1"
+    baseURL : process.env.REACT_APP_BASE_URL
 })
 export const API_KERETA = axios.create({
     baseURL : "https://booking.kai.id/api/stations2"
